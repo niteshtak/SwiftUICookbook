110738b820d3aa80f4d7f12f0797097b0b855645
niteshtak <niteshtak@outlook.com>
2021-12-05 15:30:13 +0800

    Add simple app button tests

--- SwiftUIDSTests/SwiftUIDSTests.swift
+++ SwiftUIDSTests/SwiftUIDSTests.swift
@@ -6,28 +6,19 @@
 //
 
 import XCTest
+import ViewInspector
 @testable import SwiftUIDS
 
-class SwiftUIDSTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+class AppButtonTests: XCTestCase {
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let subject = AppButton(title: "Hello SwitUI", type: .primary) {
+            print("button pressed")
         }
+        let text = try subject.inspect().button().find(text: subject.title).string()
+        XCTAssertEqual(text, "Hello SwitUI")
     }
 
 }
+
+extension AppButton: Inspectable {}
